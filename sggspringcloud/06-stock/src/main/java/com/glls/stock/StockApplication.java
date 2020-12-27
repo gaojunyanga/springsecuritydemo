2b799744b2e78df53675f0a963307640afb7120e
glls <524840158@qq.com>
2020-12-27 13:14:25 +0800

    11

---
+++ sggspringcloud/06-stock/src/main/java/com/glls/stock/StockApplication.java
@@ -0,0 +1,14 @@
+package com.glls.stock;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.glls.stock.mapper")
+public class StockApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(StockApplication.class,args);
+    }
+}
---
+++ sggspringcloud/06-stock/src/main/java/com/glls/stock/controller/StockController.java
@@ -0,0 +1,7 @@
+package com.glls.stock.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StockController {
+}
