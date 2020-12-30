5d922fdfb296604e71f336b62e1ca55187da9a52
glls <524840158@qq.com>
2020-12-30 14:02:29 +0800

    11

--- sggspringcloud/04-gateway/src/main/java/com/glls/gateway/GatewayConfiguration.java
+++ sggspringcloud/04-gateway/src/main/java/com/glls/gateway/GatewayConfiguration.java
@@ -1,6 +1,8 @@
 package com.glls.gateway;
 
 import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -9,8 +11,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.reactive.result.view.ViewResolver;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
+import javax.annotation.PostConstruct;
 import java.util.Collections;
 import java.util.List;
 
@@ -38,4 +44,18 @@ public class GatewayConfiguration {
     public GlobalFilter sentinelGatewayFilter() {
         return new SentinelGatewayFilter();
     }
+
+    @PostConstruct
+    public void init(){
+        GatewayCallbackManager.setBlockHandler(new BlockRequestHandler() {
+            @Override
+            public Mono<ServerResponse> handleRequest(ServerWebExchange serverWebExchange, Throwable throwable) {
+
+                Mono<ServerResponse> body = ServerResponse.ok().body(Mono.just("{\"error\":\"gateway流控返回\"}"), String.class);
+                return body;
+            }
+        });
+    }
+
+
 }
