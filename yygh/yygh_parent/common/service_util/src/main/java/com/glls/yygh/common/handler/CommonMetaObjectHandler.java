63984d8deb614660c7de056c6e0eeb891969e200
glls <524840158@qq.com>
2021-03-14 19:21:31 +0800

    1

---
+++ yygh/yygh_parent/common/service_util/src/main/java/com/glls/yygh/common/handler/CommonMetaObjectHandler.java
@@ -0,0 +1,27 @@
+package com.glls.yygh.common.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @ClassName : CommonMetaObjectHandler
+ * @Author : glls
+ * @Date: 2021/3/13 21:18
+ * @Description :
+ */
+@Component
+public class CommonMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+
+}
