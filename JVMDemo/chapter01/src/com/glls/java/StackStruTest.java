b4179bbc7ef149da29cd6780fe7605f479afd912
glls <524840158@qq.com>
2021-03-14 18:33:35 +0800

    1

---
+++ JVMDemo/chapter01/src/com/glls/java/StackStruTest.java
@@ -0,0 +1,16 @@
+package com.glls.java;
+
+/**
+ * @ClassName : StackStruTest
+ * @Author : glls
+ * @Date: 2021/1/29 10:01
+ * @Description : 测试栈结构
+ */
+public class StackStruTest {
+    public static void main(String[] args) {
+        //int i = 2+3;
+        int i = 2;
+        int j = 3;
+        int k = i+j;
+    }
+}
